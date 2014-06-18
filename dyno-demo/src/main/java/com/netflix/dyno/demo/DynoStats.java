084734919280f47d6510de865d80ec167a3512f3
Puneet Oberai <poberai@netflix.com>
2014-06-18 14:49:24 -0700

    Adding in percentile latencies for load test

--- dyno-demo/src/main/java/com/netflix/dyno/demo/DynoStats.java
+++ dyno-demo/src/main/java/com/netflix/dyno/demo/DynoStats.java
@@ -7,7 +7,6 @@ import com.netflix.servo.DefaultMonitorRegistry;
 import com.netflix.servo.annotations.DataSourceType;
 import com.netflix.servo.annotations.Monitor;
 import com.netflix.servo.monitor.Monitors;
-import com.netflix.servo.monitor.Timer;
 
 public class DynoStats {
 
@@ -18,8 +17,11 @@ public class DynoStats {
 	private final AtomicLong cacheHits = new AtomicLong(0L);
 	private final AtomicLong cacheMiss = new AtomicLong(0L);
 
-	private final Timer writeTimer = Monitors.newTimer("writeLatency", TimeUnit.MILLISECONDS);
-	private final Timer readTimer = Monitors.newTimer("readLatency", TimeUnit.MILLISECONDS);
+//	private final Timer writeTimer = Monitors.newTimer("writeLatency", TimeUnit.MILLISECONDS);
+//	private final Timer readTimer = Monitors.newTimer("readLatency", TimeUnit.MILLISECONDS);
+
+	private final DynoTimer writeTimer = new DynoTimer(1000, 20, 1200);
+	private final DynoTimer readTimer = new DynoTimer(1000, 20, 1200);
 
 	public static DynoStats getInstance() {
 		return Instance;
@@ -63,6 +65,57 @@ public class DynoStats {
 	@Monitor(name="cacheMiss", type=DataSourceType.COUNTER)
 	public long getCacheMiss() {
 		return cacheMiss.get();
+	}
+
+	@Monitor(name="readLatAvg", type=DataSourceType.GAUGE)
+	public double getReadLatAvg() {
+		return readTimer.getAvgMillis();
+	}
+
+	@Monitor(name="readLatP50", type=DataSourceType.GAUGE)
+	public double getReadLatP50() {
+		return readTimer.getP50Millis();
+	}
+
+	@Monitor(name="readLatP99", type=DataSourceType.GAUGE)
+	public double getReadLatP99() {
+		return readTimer.getP99Millis();
+	}
+
+	@Monitor(name="readLatP995", type=DataSourceType.GAUGE)
+	public double getReadLatP995() {
+		return readTimer.getP995Millis();
+	}
+
+	@Monitor(name="readLatP999", type=DataSourceType.GAUGE)
+	public double getReadLatP999() {
+		return readTimer.getP999Millis();
+	}
+
+	
+	@Monitor(name="writeLatAvg", type=DataSourceType.GAUGE)
+	public double getWriteLatAvg() {
+		return writeTimer.getAvgMillis();
+	}
+
+	@Monitor(name="writeLatP50", type=DataSourceType.GAUGE)
+	public double getWriteLatP50() {
+		return writeTimer.getP50Millis();
+	}
+
+	@Monitor(name="writeLatP99", type=DataSourceType.GAUGE)
+	public double getWriteLatP99() {
+		return writeTimer.getP99Millis();
+	}
+
+	@Monitor(name="writeLatP995", type=DataSourceType.GAUGE)
+	public double getWriteLatP995() {
+		return writeTimer.getP995Millis();
+	}
+
+	@Monitor(name="writeLatP999", type=DataSourceType.GAUGE)
+	public double getWriteLatP999() {
+		return writeTimer.getP999Millis();
 	}
 
 	private float getCacheHitRatio() {
