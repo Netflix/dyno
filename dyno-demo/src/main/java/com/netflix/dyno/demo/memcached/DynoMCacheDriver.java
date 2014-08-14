284a3a0d6d5f78a7a92e61107c761d818c9e0296
Puneet Oberai <poberai@netflix.com>
2014-08-14 10:32:50 -0700

    Removing unnecessary files from dyno-demo package

--- dyno-demo/src/main/java/com/netflix/dyno/demo/memcached/DynoMCacheDriver.java
+++ dyno-demo/src/main/java/com/netflix/dyno/demo/memcached/DynoMCacheDriver.java
@@ -1,49 +1,43 @@
 package com.netflix.dyno.demo.memcached;
 
-import java.util.concurrent.atomic.AtomicReference;
 
-import com.netflix.dyno.connectionpool.impl.ConnectionPoolConfigurationImpl;
-import com.netflix.dyno.contrib.EurekaHostsSupplier;
-import com.netflix.dyno.demo.DynoDriver;
-import com.netflix.dyno.memcache.DynoMCacheClient;
-
-public class DynoMCacheDriver extends DynoDriver {
-
-	private static final DynoDriver Instance = new DynoMCacheDriver();
-	
-	private AtomicReference<DynoMCacheClient> client = new AtomicReference<DynoMCacheClient>(null);
-
-	public static DynoDriver getInstance() {
-		return Instance;
-	}
-	
-	private DynoMCacheDriver() {
-		super();
-	}
-	
-	public DynoClient dynoClientWrapper = new DynoClient () {
-
-		@Override
-		public void init() {
-			 client.set(DynoMCacheClient.Builder.withName("Demo")
-						.withDynomiteClusterName("dynomite_memcached_puneet")
-						.withConnectionPoolConfig(new ConnectionPoolConfigurationImpl("dynomite_memcached_puneet")
-												  .setPort(8102))
-						.build());
-		}
-
-		@Override
-		public String get(String key) throws Exception {
-			return client.get().<String>get(key).getResult();
-		}
-
-		@Override
-		public void set(String key, String value) {
-			client.get().<String>set(key, value);
-		}
-	};
-	
-	public DynoClient getDynoClient() {
-		return dynoClientWrapper;
-	}
-}
+//public class DynoMCacheDriver extends DynoDriver {
+//
+//	private static final DynoDriver Instance = new DynoMCacheDriver();
+//	
+//	private AtomicReference<DynoMCacheClient> client = new AtomicReference<DynoMCacheClient>(null);
+//
+//	public static DynoDriver getInstance() {
+//		return Instance;
+//	}
+//	
+//	private DynoMCacheDriver() {
+//		super();
+//	}
+//	
+//	public DynoClient dynoClientWrapper = new DynoClient () {
+//
+//		@Override
+//		public void init() {
+//			 client.set(DynoMCacheClient.Builder.withName("Demo")
+//						.withDynomiteClusterName("dynomite_memcached_puneet")
+//						.withConnectionPoolConfig(new ConnectionPoolConfigurationImpl("dynomite_memcached_puneet")
+//												  .setPort(8102))
+//						.build());
+//		}
+//
+//		@Override
+//		public String get(String key) throws Exception {
+//			return client.get().<String>get(key).getResult();
+//		}
+//
+//		@Override
+//		public void set(String key, String value) {
+//			client.get().<String>set(key, value);
+//		}
+//	};
+//	
+//	public DynoClient getDynoClient() {
+//		return dynoClientWrapper;
+//	}
+//}
