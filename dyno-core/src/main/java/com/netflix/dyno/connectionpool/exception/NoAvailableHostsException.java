ce5cf4a20c36b7181ca938bed8af4a08d824b148
Ioannis Papapanagiotou <iduckhd@hotmail.com>
2016-08-09 14:50:12 -0700

    Serialized UID in NoAvailableHostsException

--- dyno-core/src/main/java/com/netflix/dyno/connectionpool/exception/NoAvailableHostsException.java
+++ dyno-core/src/main/java/com/netflix/dyno/connectionpool/exception/NoAvailableHostsException.java
@@ -2,6 +2,9 @@ package com.netflix.dyno.connectionpool.exception;
 
 public class NoAvailableHostsException extends DynoConnectException {
 
+
+	private static final long serialVersionUID = -6345231310492496030L;
+
 	public NoAvailableHostsException(String message) {
 		super(message);
 	}
