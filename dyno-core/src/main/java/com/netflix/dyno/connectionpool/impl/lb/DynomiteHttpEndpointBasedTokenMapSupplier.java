6f1c9f8fcd808fb3d06634ac9779118480c138d4
Rodolfo Ribeiro Silva <rrsilva@uolinc.com>
2017-01-03 21:03:54 -0200

    Change header

--- dyno-core/src/main/java/com/netflix/dyno/connectionpool/impl/lb/DynomiteHttpEndpointBasedTokenMapSupplier.java
+++ dyno-core/src/main/java/com/netflix/dyno/connectionpool/impl/lb/DynomiteHttpEndpointBasedTokenMapSupplier.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2011 Netflix
+ * Copyright 2017 Netflix
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
